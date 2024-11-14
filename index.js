a65d56ba080af536aa8bff37012fbcb111680160
Lucas Gomes <115755837+Lucasagdev@users.noreply.github.com>
2024-11-13 23:08:26 -0300

    Atualizando a quantidade de baralhos

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 document.getElementById('tirar-carta').addEventListener('click', ()=> { tirarUmaCartaAleatoriaDoBaralho()})
 
 async function criarBaralhoEmbaralhado(){
-    const url = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    const url = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6"
     const resposta = await fetch(url)
     return await resposta.json()
     
 }
 
 async function tirarUmaCarta(deck_id){
-    const url = `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`
+    const url = `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=6`
     const resposta = await fetch(url)
     return await resposta.json()
 }
